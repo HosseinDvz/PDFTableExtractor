c005042de18e428ba53262938f969ab11c607668
Hossein Davarzanisani <hosseindavarzanisani@mac.home>
2025-04-25 09:37:08 -0400

    adding validation to the main.py

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from ocr import Ocr
 from table_transformer import TableTransformer
 from table_constructor import TableConstruction
 from csv_creator import DataFrameSaver
-from anomaly_detector import AnomalyDetector
+#from anomaly_detector import AnomalyDetector
 import numpy as np
+from validator import TableDataValidator
+import os
+import dotenv
+
+
+dotenv.load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
 import os
 
@@ -24,9 +31,13 @@ dirname, _, images = list(os.walk('table_images'))[0]
 #print(images)
 images_with_path = [os.path.join(dirname, image) for image in images] # getting list of images in the table_image forlder
 
-saver = DataFrameSaver()
+saver_1= DataFrameSaver()
+
+validator = TableDataValidator(api_key)
+saver_2 = DataFrameSaver(folder_name='validated_tables', clean_folder=True)
 
 for i, image in enumerate(images_with_path):
+    print(image)
     try:
         ocr_instance = Ocr(image)  # Create an instance of the Ocr class
     except Exception: # except some sys files in the image folders
@@ -38,26 +49,25 @@ for i, image in enumerate(images_with_path):
     while True:
         try: 
             # Apply OCR with preprocessing
-            df = ocr_instance.apply_easyocr(dpi_adjustment=False,expansion=False,  dilation=False, resize_float=resize_float)
+            df = ocr_instance.apply_easyocr(dpi_adjustment=False,expansion=True,  dilation=False, resize_float=resize_float)
             table = TableConstruction(df)
             final_table = table.table_creator()
-            break
+            saver_1.save(final_table, f'Table_{i}')
+            break 
         except Exception:
             resize_float = np.random.uniform(0.29, 0.34)
-        
-        
-    print(final_table)
-    detector = AnomalyDetector(threshold=10)
-    print('+'*20)
-    print(f'possible anomalies of table {i}: \n {detector.find_large_anomalies(final_table)}')
-    print('='*40)
     
-    saver.save(final_table, f'Table_{i}')
+    corrected_df = validator.validate_table_data(image, f'csv_tables/Table_{i}.csv')
+    saver_2.save(corrected_df, f'table_{i}_validated')  
+        
+    print(corrected_df)
+    #detector = AnomalyDetector(threshold=10)
+    print('='*40)
     
     
 
 '''
-'/Users/hosseindavarzanisani/AC/Semester_2/AI Project_2/AAFC/Codes/pdfs/1-s2.0-S0308814617312839-Lentils.pdf' 
+'/pdfs/1-s2.0-S0308814617312839-Lentils.pdf' 
 apply_easyocr(dpi_adjustment=False,expansion=False,  dilation=True, resize_float=0.3)
 
 acs.jafc.7b00697 Buckwheat pinto.pdf'
